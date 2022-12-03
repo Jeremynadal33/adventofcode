406a84fd14f9f06b3f1f0c2e392b849c38d6a0e4
Jeremynadal33 <jjjnadal33@gmail.com>
2022-12-03 11:49:16 +0100

    uncommited last year

---
+++ adventofcode2021/scripts/day18.py
@@ -0,0 +1,37 @@
+
+import sys
+import numpy as np
+import time 
+from functions import *
+
+def main():
+    input_file, part = handle_args(sys.argv)
+    solve_puzzle(input_file = input_file, part = part)
+
+def solve_puzzle(input_file, part):
+    if part == 1 :
+        solve_part_1(input_file)
+    elif part == 2 :
+        solve_part_2(input_file)
+
+def solve_part_1(input_file):
+    print('Solving puzzle part 1')
+    # file = file.replace(',', ', ')
+    input_array = open(input_file).read().strip().split("\n")
+    input_array = list(input_array)
+    print(input_array)
+
+
+
+def solve_part_2(input_file):
+    print('Solving puzzle part 2')
+    file = open(input_file, 'r').read().split('\n')
+    
+
+
+
+if __name__ == "__main__":
+    start = time.time()
+    main()
+    end = time.time()
+    print('Elapsed in {} milisecondes'.format(1000*(end-start)))
